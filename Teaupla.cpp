d9c599d854d19812dfa4b1033c2301c2983c6c0e
NatGilsim <58848588+NatGilsim@users.noreply.github.com>
2019-12-13 12:52:25 +0100

    Add files via upload

---
+++ Teaupla.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+#include "Zeca.hpp"
+using namespace std;
+
+Teaupla::Teaupla(){}
---
+++ Teaupla.hpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "Zeca.hpp"
+
+#ifndef TEAUPLA
+#define TEAUPLA
+
+class Teaupla {
+  private :
+    unsigned int sizeX;
+    unsigned int sizeY;
+    Zeca[sizeX][sizeY] tab;
+  public :
+    Teaupla();
+};
+
+
+#endif
