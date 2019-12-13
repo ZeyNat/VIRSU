4a9832121284e0b765fde91bc3cdc4df97d3ddfc
ZeynebMhamedi <36275961+ZeynebMhamedi@users.noreply.github.com>
2019-12-13 14:32:47 +0100

    Add files via upload

---
+++ Zeca.hpp
@@ -0,0 +1,21 @@
+#ifndef ZECA
+#define ZECA
+//#include "Teaupla.hpp"
+
+class Oueurj;
+class Streumons;
+class Teupors;
+
+class Zeca{
+  private:
+    int X;
+    int Y;
+    //char etat;//oueurj = J / streumons = s / remus = X / diams = $ / geurchars = * /teupors = p / vide =' '
+
+
+  public:
+    Zeca();
+    Zeca(int x, int y, char e);
+};
+
+#endif
