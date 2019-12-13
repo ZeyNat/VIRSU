e69f3c310db2244bf56ab9d0349f4b553a8302f9
Natacha Gilsim <natacha@abby-x550jx.home>
2019-12-13 21:01:38 +0100

    Le Teaupla marche !

--- Teaupla.cpp
+++ Teaupla.cpp
@@ -11,14 +11,30 @@ Teaupla::Teaupla(string nomFichier){
   string line;
   unsigned int i = 0;
   int maxX = 0;
+  vector<Zeca> v;
     //lecture fichier et init des Zecas
     while(getline(f,line)){
       for (unsigned int j = 0; j < line.length(); j++){
-        tab[i].push_back(Zeca(i,j,line.at(j)));
+        v.push_back(Zeca(i,j,line.at(j)));
       }
       maxX = max(maxX,(int)line.length());   //je prends le max des largeurs
       i++;
+      tab.push_back(v);
+      v.clear();
     }
-    sizeY = i+1;   // on sauvegarde la taille du platea ca servira surement
-    sizeX = (unsigned int) maxX + 1;
+    sizeY = i;   // on sauvegarde la taille du teaupla ca servira surement
+    sizeX = (unsigned int) maxX;
+
+}
+
+
+
+void Teaupla::affiche(){
+  for (unsigned int i = 0 ; i < sizeY; i++){
+    for (Zeca z : tab[i]){
+      cout << z.getEtat();
+    }
+    cout << endl;
+  }
+
 }
