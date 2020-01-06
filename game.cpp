ed5e2191a94cd4589a6922c1a7e36b9d8564b3ee
NatGilsim <nat.gilsim@gmail.com>
2020-01-06 17:48:57 +0100

    game

---
+++ game.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <time.h>
+#include <fstream>
+#include "board.hpp"
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+
+int main(int argc, char **argv){
+  string nom = argv[1];
+  nom += ".game";
+  int nbTeauplas = (int) *argv[2] -48;
+  ofstream flux (nom.c_str());
+  string nomBoard;
+  char num = '1';
+  for (int i = 1; i < nbTeauplas; i++){
+    nomBoard = "b";
+    nomBoard += num;
+    nomBoard += ".board";
+    num++;
+    cout << nomBoard << endl;
+    createBoard(nomBoard);
+    flux << nomBoard << endl;
+  }
+
+
+}
